990eec63003640a389210dcb4b51fedf8c4a776c
sherri010 <aminshahidi.sh@gmail.com>
2017-03-06 14:36:27 -0800

    adds all crud for developer + views

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,19 @@
 class DevelopersController < ApplicationController
 
+
+  def new
+    @dev = Developer.new
+  end
+  def create
+    @dev = Developer.new(developer_params)
+    if @dev.save
+      flash[:success]="New member added"
+      redirect_to developers_url
+    else
+      flash[:error]="Fialed adding new member ... please try again"
+      redirect_to :back
+    end
+  end
   def index
     @devs = Developer.all
   end
@@ -9,13 +23,38 @@ class DevelopersController < ApplicationController
   end
 
   def edit
-    
+    @dev = Developer.find(params[:id])
   end
 
   def update
+    @dev = Developer.find(params[:id])
+    @dev.update_attributes(developer_params)
+    if @dev.save
+      flash[:success] = "Successful update"
+      redirect_to developers_url
+    else
+      flash[:error] = "Failed update...please try again"
+      redirect_to :back
+    end
   end
 
   def destroy
+    @dev.find(params[:id])
+    if @dev.destroy
+      flash[:success]="Successfully removed developer from the team"
+      redirect_to developers_url
+    else
+      flash[:error]="failed removing developer from the team... please try again"
+      redirect_to :back
+    end
   end
+
+
+  private
+
+  def developer_params
+    params.require(:developer).permit(:first_name, :last_name, :email , :years_experience)
+  end
+
 
 end
